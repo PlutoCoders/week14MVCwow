aa6fd83131f867d4805369923f21b3620923de83
Alux <plutocoding@gmail.com>
2023-12-12 07:00:26 -0500

    sign js and custom alerts

---
+++ public/js/signup.js
@@ -0,0 +1,25 @@
+const signupFormHandler = async function(event) {
+  event.preventDefault();
+
+  const usernameElement = document.querySelector('#username-sign');
+  const passwordElement = document.querySelector('#password-sign');
+
+  const response = await fetch('/api/user', {
+    method: 'POST',
+    body: JSON.stringify({
+      username: usernameElement.value,
+      password: passwordElement.value,
+    }),
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  if (response.ok) {
+    document.location.replace('/dashboard');
+  } else {
+    showAlert(`Failed to Signup`);
+  }
+};
+
+document
+  .querySelector('#sign-form')
+  .addEventListener('submit', signupFormHandler);
