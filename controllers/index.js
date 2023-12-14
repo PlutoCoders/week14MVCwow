5a68ac79f4d808ec18e6739aea1feb5d15f69f2e
Alux <plutocoding@gmail.com>
2023-12-13 21:34:10 -0500

    fixed home routes link up in index.js

--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 // router for events(Post Put Delete) + Homepage (Get Routes)
 
 const apiRoutes = require('./api');
-const homeRoutes = require('./home-routes.js');
+const homeRoutes = require('./homeRoutes.js');
 const dashboardRoutes = require('./dashboard-routes.js');
 
 router.use('/', homeRoutes);
