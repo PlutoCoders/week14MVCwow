fdfd5363bab935fc992eb8defb4515b02412dc3d
Alux <plutocoding@gmail.com>
2024-01-09 14:04:47 -0500

    setting up post route structure, installing more packages

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -14,6 +14,23 @@ router.post('/', withAuth, async (req, res) => {
 });
 
 // Setup Put Route
+router.put('/:id', withAuth, async (req, res) => {
+  try {
+    const [affectedRows] = await Post.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (affectedRows > 0) {
+      res.status(200).end();
+    } else {
+      res.status(404).end();
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // Setup delete Route
 
